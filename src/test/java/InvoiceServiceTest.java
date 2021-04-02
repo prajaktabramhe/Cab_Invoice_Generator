e941ef4478fcfe12d05c09b56bd953b67d077608
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-02 12:25:26 +0530

    [Prajakta]: Added: Calculated fare and multiple rides

---
+++ src/test/java/InvoiceServiceTest.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceServiceTest
+{
+    @Test
+    public void givenDistaanceAndTime_ShouldReturnTotalFare()
+    {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare()
+    {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare( distance, time);
+        Assertions.assertEquals(5, fare, 0.0);
+    }
+@Test
+    public  void  givenMultipleRides_shouldReturnTotalFare()
+{
+    InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+    Ride[] rides = {
+                    new Ride(2.0 , 5),
+                    new Ride(0.1 , 1)
+
+                   };
+    double fare = invoiceGenerator.calculateFare(rides);
+    Assertions.assertEquals(30, fare, 0.0);
+}
+}
