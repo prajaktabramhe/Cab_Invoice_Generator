5c695d2a6dd7d091625c64e7e799424f4ad45611
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-02 15:15:40 +0530

    [Prajakta]: Invoice generator should return total number of rides and total fare and average fare per ride

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,9 +1,11 @@
-public class InvoiceGenerator {
+public class InvoiceGenerator
+{
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final double MINIMUM_FARE = 5;
 
-    public double calculateFare(double distance, int time) {
+    public double calculateFare(double distance, int time)
+    {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
@@ -11,9 +13,10 @@ public class InvoiceGenerator {
     public InvoiceSummary calculateFare(Ride[] rides)
     {
         double totalFare = 0;
-        for (Ride ride : rides) {
+        for (Ride ride : rides)
+        {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
-        return null;
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
--- src/main/java/InvoiceSummary.java
+++ src/main/java/InvoiceSummary.java
@@ -1,7 +1,7 @@
 public class InvoiceSummary {
     private final int numberOfRides;
     private final double totalFare;
-    private final Object averageFare;
+    private final double averageFare;
 
     public InvoiceSummary(int numberOfRides, double totalFare)
     {
@@ -10,5 +10,12 @@ public class InvoiceSummary {
          this.averageFare = this.totalFare/this.numberOfRides;
     }
 
-
+    @Override
+    public  boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return  numberOfRides == that.numberOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.averageFare, averageFare) == 0;
+    }
 }
